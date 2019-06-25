a68ae6a4523b54b680d182da544fed90f21d892f
Jose Pando <josepando@MacBook-Pro-de-Jose.local>
2019-06-24 21:59:02 -0500

    aa

--- impresion.php
+++ impresion.php
@@ -10,53 +10,44 @@ $ventasGeneral=$sale->getVenta($_GET['codigo']);
 $stylesheet = file_get_contents('bootstrap4.3.min.css');
 $name="LocalSucre ".$_GET['codigo'].".pdf";
 $tipoText="Venta en el local";
+$pagocon=$_GET['pagocon'];
+$vuelto=$_GET['vuelto'];
+if($_GET['pedido']){
+  $ventaGeneral=$pedidosClass->getPedidoAdmin($codigo);
+  $cliente=true;
+}else{
+  $ventaGeneral=$ventasClass->getVenta($codigo);
+}
 $html="
-
     <table>
-      <tr>
-        <td><img src='../../assets/img/logo-inv.png'/></td>
-      </tr>
-    <tr>
-      <td class='table'><h2>LocalSucre</td>
-    </tr>
-    <tr>
-      <td class='table'>Av. Sucre 1051, Pueblo Libre.</td>
-    </tr>
-      <tr>
-        <td class='table'>Venta ".$ventasGeneral['metodoVenta'].".
-        </td>
-      </tr>
-      <tr>
-        <td class='table'>Pag&oacute; con ".$ventasGeneral['metodoPago'].".
-        </td>
-      </tr>
-      <tr>
-        <td class='table'>Fecha: ".$ventasGeneral['fecha']." "." Hora: ".$ventasGeneral['hora'].".
-      </td>
-      <tr>
-        <td class='table'>Hecha por: ".$ventasGeneral['usuario'].".
-      </td>
-      </tr>
+    <tr></tr>
+    <tr><td class='table'><h2>LICORERIA SUCRE</td></tr>
+    <tr><td class='table'>TELEFONO:(01) 480-0167  943 727 483.</td></tr>
+    <tr><td class='table'>DIRECCION: SUCRE S/N P. LIBRE ".$ventasGeneral['metodoVenta'].".</td></tr>
+    <tr><td class='table'>".date('d/m/y')." ".date('H:i:s').".</td>";
+    if($cliente){
+      $html.="<tr><td class='table'>DIRECCION: ".strtoupper($ventaGeneral['direccion']).".</td></tr>
+              <tr><td class='table'>NOMBRE: ".strtoupper($ventaGeneral['nombre'])." ".$ventaGeneral['telefono']."</td></tr>
+              <tr><td class='table'>OBSERVACION: ".strtoupper($ventaGeneral['observaciones'])."</td></tr>";
+    }
+    $html.="
     </table>
     <table>
       <thead>
         <tr>
-        <th class='align'>Cantidad</th>
-        <th>Producto</th>
-        <th>Precio de venta</th>
-        <th>Sub-Total</th>
+        <th class='align'>CANT</th>
+        <th>ARTICULO</th>
+        <th>PRECIO</th>
         </tr>
       </thead>
       <tbody>";
 
       foreach ($ventas as $venta) {
-        $subtotal = $venta['cantidad']*$venta['precio'];
         $total+=$subtotal;
         $html.="<tr>
                   <td class='align'>".$venta['cantidad']."</td>
-                  <td>".$venta['producto']."</td>
+                  <td>".strtoupper($venta['producto'])."</td>
                   <td>".money_format('%(#10n',$venta['precio'])."</td>
-                  <td>".money_format('%(#10n',$subtotal)."</td>
               </tr>";
       }
       $html.="<tr><th class='total align'>Total</th><th class='total'>".money_format('%(#10n',$total)."</th></tr>
